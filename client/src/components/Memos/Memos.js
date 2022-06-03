dd0cfe3208782f426771b8b1488e6d91d54b626c
The Privacy Dev <theprivacydev@protonmail.com>
2022-06-02 23:48:42 -0500

    memos now display correctly from MongoDB database and a message appears telling a user to create a memo when there are none

--- client/src/components/Memos/Memos.js
+++ client/src/components/Memos/Memos.js
@@ -2,27 +2,28 @@ import React from "react";
 import { ListGroup, ListGroupItem } from "react-bootstrap";
 
 function Memos({ memos }) {
-  console.log(memos);
+  const memoList = memos.map((memo, index) => {
+    return (
+      <ListGroupItem key={index} className="text-light bg-dark border-info">
+        <strong>{memo.title}</strong>
+        <div>
+          <small>Unique Id: {memo["_id"]}</small>
+          <small>{memo.date}</small>
+        </div>
+      </ListGroupItem>
+    );
+  });
   return (
     <aside
       style={{ height: "110vh", width: "35vw" }}
       className="border border-info"
     >
       <ListGroup>
-        {memos.map((memo, index) => {
-          return (
-            <ListGroupItem
-              key={index}
-              className="text-light bg-dark border-info"
-            >
-              <strong>{memo.title}</strong>
-              <div>
-                <small>Unique Id: {memo["_id"]}</small>
-                <small>{memo.date}</small>
-              </div>
-            </ListGroupItem>
-          );
-        })}
+        {memos.length !== 0 ? (
+          memoList
+        ) : (
+          <ListGroupItem className="text-light bg-dark">Create a memo to get started!</ListGroupItem>
+        )}
       </ListGroup>
     </aside>
   );
