4d70b097a7c7983b59fcc1dab7df864cd54be849
The Privacy Dev <theprivacydev@protonmail.com>
2022-06-04 03:34:37 -0500

    tried removing local host from port to see if it works for deployment

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const dotenv = require("dotenv").config();
 const apiRoutes = require("./routes/api-routes.js");
 const cors = require("cors");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 
 const app = express();
 
