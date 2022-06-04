103090302eab91ae9001443f6e0df520bb1ca8c7
The Privacy Dev <theprivacydev@protonmail.com>
2022-06-04 14:56:40 -0500

    removed res.status in api-routes to prevent header error in production

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,7 +4,7 @@ const Memo = require("../models/Memo");
 router.get("/memos", async (req, res) => {
   try {
     const memos = await Memo.find();
-    return res.status(200).json(memos);
+    return res.json(memos);
   } catch (err) {
     res.json(err);
     res.status(500);
@@ -19,7 +19,7 @@ router.post("/memos", async (req, res) => {
       content: req.body.content
     });
     const saveMemo = await newMemo.save();
-    return res.status(201).json(saveMemo);
+    return res.json(saveMemo);
   } catch (err) {
     res.json(err);
     res.status(501);
@@ -37,7 +37,7 @@ router.put("/memos/:id", async (req, res) => {
       },
       { new: true }
     );
-    return res.status(200).json(updateMemo);
+    return res.json(updateMemo);
   } catch (err) {
     res.json(err);
     res.status(501);
