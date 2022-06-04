dd6ef1ce12219a7f43980a798883c55c28f717c0
The Privacy Dev <theprivacydev@protonmail.com>
2022-06-04 03:39:07 -0500

    added 80 port which is default for heroku to solve binding to port issue

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const dotenv = require("dotenv").config();
 const apiRoutes = require("./routes/api-routes.js");
 const cors = require("cors");
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 80;
 
 const app = express();
 
